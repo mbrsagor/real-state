0aa563b797323bde2fb44ee96a1e7623de928aee
Sagor <mbrsagor@gmail.com>
2021-01-29 18:02:57 +0600

    Serializer issues fixed

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'content', 'is_save',
