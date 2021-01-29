c233bf070c5a98c5ce5908e952236444dfe3d073
Sagor <mbrsagor@gmail.com>
2021-01-29 17:30:57 +0600

    Refactor

--- backend/contacts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ContactCreateView
-
-urlpatterns = [
-    path('message', ContactCreateView.as_view()),
-]
--- backend/listings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ListingsView, ListingView, SearchView
-
-urlpatterns = [
-    path('list-item', ListingsView.as_view()),
-    path('search', SearchView.as_view()),
-    path('<slug>', ListingView.as_view()),
-]
