fcce76728b38818291ea9806f26d2ce65273ba5e
Sagor <mbrsagor@gmail.com>
2021-01-31 23:54:59 +0600

    Todo detail page static content

--- todo-frontned/src/components/Tododetails.jsx
+++ todo-frontned/src/components/Tododetails.jsx
@@ -1,26 +1,20 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { todoDetail } from "../store/actions/todoAction";
+import React, { Component } from 'react'
 
 class Tododetails extends Component {
-  componentDidMount() {
-    document.title = "Todo Detail";
-    // this.props.todoDetail(this.props.match.params.id);
-  }
-  render() {
-    const { todo } = this.props;
-    console.log(todo);
-    return (
-      <div>
-        <h4 className="card-title">Title </h4>
-        <p className="description"></p>
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div>
+            <h2>Todo detail page</h2>
+            <h4 className="title">This is a new todo title</h4>
+            <p>
+              Intrinsicly revolutionize 2.0 benefits after pandemic outsourcing.
+              Progressively fashion client-centered meta-services vis-a-vis
+              synergistic relationships. Dynamically aggregate best-of-breed
+              information for customized benefits.
+            </p>
+          </div>
+        );
+    }
 }
 
-const mapStateToProps = (state) => ({
-  todo: state.todos.todo,
-});
-
-export default connect(mapStateToProps, { todoDetail })(Tododetails);
+export default Tododetails
